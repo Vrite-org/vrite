2ecfea205cd4652284ac2b7bcd32a504dd8846ee
Arqam Zia <arqam.mzia@gmail.com>
2024-12-01 03:00:53 +0500

    mix new: hello world

---
+++ lib/vrite.ex
@@ -0,0 +1,18 @@
+defmodule Vrite do
+  @moduledoc """
+  Documentation for `Vrite`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Vrite.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/vrite_test.exs
@@ -0,0 +1,8 @@
+defmodule VriteTest do
+  use ExUnit.Case
+  doctest Vrite
+
+  test "greets the world" do
+    assert Vrite.hello() == :world
+  end
+end
